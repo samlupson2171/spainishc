5332cbcd9b63d68a4e96e1a22be143198de14fa4
sam-1413 <sam@quoteawayai.com>
2026-03-07 22:44:32 +0100

    Remove variant prop from LeadWizard usage

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
               </div>
             </div>
             <div>
-              <LeadWizard variant="hero" />
+              <LeadWizard />
             </div>
           </div>
         </div>
